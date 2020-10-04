c4a0d43fa05b3de6280d27aa12a4e3172516e12b
Kunihito Mitsuboshi <mitsuboshi20190809@gmail.com>
2020-10-04 19:18:59 +0900

    add serial

--- Makefile
+++ Makefile
@@ -2,13 +2,13 @@ hw	: hw.c
 	gcc -o hw hw.c
 
 kekka	: math/kekka.c
-	gcc math/kekka.c -o math/kekka -lm
+	gcc math/kekka.c -o kekka -lm
 
-kservo	: kservo.cpp
-	g++ -std=c++11 -o kservo kservo.cpp -lics3
+kservo	: serial/kservo.cpp
+	g++ -std=c++11 -o kservo serial/kservo.cpp -lics3
 
 cuda    : cuda.cu
 	nvcc -o cuda cuda.cu
 
 clean	:
-	rm hw math/kekka kservo cuda
+	rm hw kekka kservo cuda
---
+++ serial/kservo.cpp
@@ -0,0 +1,85 @@
+/*
+ *  2020.10.4
+ *  kservo.cpp
+ *  ver 0.9
+ *  Kunihito Mitsuboshi
+ *  license(Apache-2.0) at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/*
+ *  ~/tools/libics3/script/setup.sh
+ *  sudo chmod o+rw /dev/ttyUSB0
+ */
+
+#include <iostream>
+
+#include <iomanip>
+#include <vector>
+#include <cstdio>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/joystick.h>
+
+#include <stdexcept>
+#include <fstream>
+#include <chrono>
+#include <thread>
+#include <ics3/ics>
+
+#define JOY_DEV "/dev/input/js0"
+#define SERVO_DEV "/dev/ttyUSB0"
+
+main(int argc, char *argv[])
+{
+	int joy(-1), num_of_axis(0), num_of_buttons(0);
+	char name_of_joystick[80];
+	std::vector<int> joy_a;
+	std::vector<char> joy_b;
+
+	joy = open(JOY_DEV,O_RDONLY);
+
+	ioctl(joy, JSIOCGAXES, &num_of_axis);
+	joy_a.resize(num_of_axis,0);
+	ioctl(joy, JSIOCGBUTTONS, &num_of_buttons);
+	joy_b.resize(num_of_buttons,0);
+	ioctl(joy, JSIOCGNAME(80), &name_of_joystick);
+
+	std::cout << "JOYSTICK NAME : " << name_of_joystick << std::endl;
+
+
+	auto servo_id(0), servo_d(0);
+
+	ics::ICS3 servo {SERVO_DEV, ics::Baudrate::RATE115200()};
+
+	servo_id = servo.getID();
+	std::cout << "KONDO SERVO ID : " << (int)servo_id << std::endl;
+
+	while(true)
+	{
+		js_event js;
+		read(joy, &js, sizeof(js_event));
+
+		switch (js.type & ~JS_EVENT_INIT)
+		{
+		case JS_EVENT_AXIS :
+			joy_a[(int)js.number] = js.value;
+			break;
+		case JS_EVENT_BUTTON :
+			joy_b[(int)js.number] = js.value;
+			break;
+		}
+
+		std::cout << "AXIS0 : " << joy_a[0] << std::endl;
+		servo_d = (int)(joy_a[0]/300);
+
+		auto ans = servo.move(servo_id, ics::Angle::newDegree(servo_d));
+		std::cout << "POSITION : " << ans.getRaw() << std::endl;
+
+		usleep(500);
+	}
+
+	close(joy);
+
+	return(0);
+}
