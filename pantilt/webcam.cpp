c01ff60f7582cd6e9d2b224e48c60c57a1f444c2
Kunihito Mitsuboshi <mitsuboshi20190809@gmail.com>
2021-01-28 07:27:34 +0900

    add

---
+++ pantilt/webcam.cpp
@@ -0,0 +1,20 @@
+/*
+ *  2021.1.30
+ *  webcam.cpp
+ *  ver 1.0
+ *  Kunihito Mitsuboshi
+ *  license(Apache-2.0) at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <thread>
+
+
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
