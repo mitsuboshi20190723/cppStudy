05bd461c411bc7c4a96fb7bb990d4e3eb9c61ee8
Kunihito Mitsuboshi <mitsuboshi20190809@gmail.com>
2021-01-30 09:15:16 +0900

    add

--- pantilt/webcam.cpp
+++ pantilt/webcam.cpp
@@ -1,20 +1,36 @@
 /*
  *  2021.1.30
  *  webcam.cpp
- *  ver 1.0
+ *  ver 0.5
  *  Kunihito Mitsuboshi
  *  license(Apache-2.0) at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-#include <iostream>
-#include <vector>
-#include <chrono>
-#include <thread>
+/*
+ * for picam
+ * echo "bcm2835-v4l2" >> /etc/modules
+ * ls /dev/video*
+ *
+ * for openCV 4.5
+ */
 
 
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
 
 int main(int argc, char **argv)
 {
+	VideoCapture cap(0);
+	if(!cap.isOpened()) return -1;
 
+	Mat frame;
+	namedWindow("camera", WINDOW_AUTOSIZE);
+	for(;;)
+	{
+		cap >> frame;
+		imshow("camera", frame);
+		if(waitKey(30) >= 0) break;
+	}
 	return 0;
 }
