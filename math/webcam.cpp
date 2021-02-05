f8f8caa1757aea5249701074b8041bfd58402b16
Kunihito Mitsuboshi <mitsuboshi20190809@gmail.com>
2021-02-06 08:08:34 +0900

    add

--- math/webcam.cpp
+++ math/webcam.cpp
@@ -1,20 +1,22 @@
 /*
- *  2021.2.2
+ *  2021.2.5
  *  webcam.cpp
- *  ver 0.5
+ *  ver 1.0
  *  Kunihito Mitsuboshi
  *  license(Apache-2.0) at http://www.apache.org/licenses/LICENSE-2.0
  */
 
 /*
- * for picam
+ * for raspicam
  * echo "bcm2835-v4l2" >> /etc/modules
+ * reboot
  * ls /dev/video*
  *
  * for openCV 4.5
  */
 
 #include <iostream>
+#include <vector>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 
@@ -23,29 +25,44 @@
 int main(int argc, char **argv)
 {
 	cv::VideoCapture cap(0);
-	if(!cap.isOpened()) return -1;
-
 	int t;
 	double w, h, f;
+	cv::CascadeClassifier cascade;
+	cv::Mat img;
+	std::vector<cv::Rect> faces;
+	cv::Rect face;
+
+	if(!cap.isOpened()) return -1;
+
 	w = cap.get(cv::CAP_PROP_FRAME_WIDTH);
 	h = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
 	f = cap.get(cv::CAP_PROP_FPS);
 	t = cap.get(cv::CAP_PROP_FORMAT);
 	std::cout << "w=" << w << ",h=" << h << ",fps=" << f << std::endl;
 	std::cout << "FORMAT=" << t << std::endl;
-
 	cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
 	cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
 	cap.set(cv::CAP_PROP_FPS, 30);
 	cap.set(cv::CAP_PROP_FORMAT, 2);
 
-	cv::Mat img;
-	cv::namedWindow("camera", cv::WINDOW_AUTOSIZE);
+	cascade.load("/usr/local/share/opencv4/haarcascades/haarcascade_frontalface_alt.xml");
+//	cascade.load("/usr/local/share/opencv4/haarcascades/haarcascade_frontalcatface.xml");
+
+//	cv::namedWindow("camera", cv::WINDOW_AUTOSIZE);
 	for(;;)
 	{
 		cap >> img;
+
+		cascade.detectMultiScale(img, faces, 1.1, 3, 0, cv::Size(50, 50));
+		for(int i=0; i<faces.size(); i++) cv::rectangle(img, cv::Point(faces[i].x, faces[i].y), cv::Point(faces[i].x+faces[i].width, faces[i].y+faces[i].height), cv::Scalar(0,0,255), 3);
+
 		cv::imshow("camera", img);
-		if(cv::waitKey(30) >= 0) break;
+
+//		if(cv::waitKey(30) >= 0) break;
+		const int key = cv::waitKey(1);
+		if(key == 'q') break; // 113
 	}
+	cv::destroyAllWindows();
+
 	return 0;
 }
--- pantilt/pantilt4axis.cpp
+++ pantilt/pantilt4axis.cpp
@@ -1,7 +1,7 @@
 /*
  *  2021.2.6
- *  pantilt.cpp
- *  ver 1.8
+ *  pantilt4axis.cpp
+ *  ver 1.0
  *  Kunihito Mitsuboshi
  *  license(Apache-2.0) at http://www.apache.org/licenses/LICENSE-2.0
  */
@@ -14,17 +14,12 @@
 
 
 #define JOY_DEV "/dev/input/js0"
-#define VIDEO_DEV "/dev/video0"
 #define SERVO_DEV "/dev/ttyUSB0"
 
 
 #define AXIS_NEUTRAL 0
 #define AXIS_PERSTEP 0.0039 /* -32,768(-127.8)   0(0)   32,767(127.8) */
 
-#define IMGX_NEUTRAL 320
-#define IMGX_PERSTEP 0.1
-#define IMGY_NEUTRAL 240
-#define IMGY_PERSTEP 0.1 /*?*/
 
 #define SERVO_NEUTRAL 7500
 #define SERVO_PERSTEP 0.034 /* 3,500(-135)   7,500(0)   11,500(135) */
@@ -34,20 +29,24 @@ int main(int argc, char **argv)
 {
 	joystick jd;
 	icsservo sd;
-	webcam vd;
-	motion pan, tilt;
+	motion pan, tilt/*, grad*/;
 
 	if(jd.connect(JOY_DEV) < 0) { std::cout << "Do not open " << JOY_DEV << std::endl; return -1; }
 	if(sd.connect(SERVO_DEV) < 0) { std::cout << "Do not open " << SERVO_DEV << std::endl; return -1; }
 
 	pan.set_motion(jd.axis(0), AXIS_NEUTRAL, AXIS_PERSTEP*0.5, 1, SERVO_NEUTRAL, SERVO_PERSTEP);
 	tilt.set_motion(jd.axis(1), AXIS_NEUTRAL, AXIS_PERSTEP*-0.25, 2, SERVO_NEUTRAL, SERVO_PERSTEP);
+//	grad.set_motion(jd.axis(3), AXIS_NEUTRAL, AXIS_PERSTEP*0.75, 3, SERVO_NEUTRAL, SERVO_PERSTEP);
 
 	while(true)
 	{
 		sd.set_pos( jd.get_ctl( &pan ) );
 
 		sd.set_pos( jd.get_ctl( &tilt ) );
+
+//		jd.get_ctl( &grad );
+//		std::cout << grad.a << std::endl;
+//		sd.set_pos( &grad );
 
 		std::this_thread::sleep_for(std::chrono::microseconds(1000));
 	}
