1496f345ba7923209664677c63eeca76e934a852
mitsuboshi20190723 <53225096+mitsuboshi20190723@users.noreply.github.com>
2024-05-12 08:13:51 +0900

    Add files via upload

---
+++ serial/hb.c
@@ -0,0 +1,25 @@
+/*
+ *  2024.5.12
+ *  hb.c
+ *  ver 1.0
+ *  Kunihito Mitsuboshi
+ *  license(Apache-2.0) at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main()
+{
+	int heartbeat;
+
+	for (heartbeat=1;;heartbeat=++heartbeat%2)
+	{
+		printf("%d\n", heartbeat);
+		usleep(500000); /* sleep(0.5); <- error */
+	}
+
+	return(0);
+}
