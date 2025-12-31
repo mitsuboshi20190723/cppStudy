848dd44d2ca730ce6d22334f3f969fe539fb6ba7
mitsuboshi20190723 <53225096+mitsuboshi20190723@users.noreply.github.com>
2026-01-01 05:30:54 +0900

    Add files via upload

---
+++ hello_world/hw.rs
@@ -0,0 +1,14 @@
+/*
+ * 2026.1.1
+ * hw.rs
+ * ver.1.1
+ * Kunihito Mitsuboshi
+ * license(Apache-2.0) at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+
+fn main()
+{
+	println!("Hello, world!");
+//	println!(); print!("Hello, world!\n");
+}
